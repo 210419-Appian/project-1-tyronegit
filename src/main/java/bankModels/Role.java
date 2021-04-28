29ba6af33d604292162a574befb2f7ba1d6a6a74
tyronegit <f35a@hotmail.com>
2021-04-28 17:28:32 -0400

    learning how

---
+++ src/main/java/bankModels/Role.java
@@ -0,0 +1,39 @@
+package bankModels;
+
+// **Role**
+//The Role model keeps track of user permissions. Can be expanded for more features later. Could be `Admin`, `Employee`, `Standard`, or `Premium`
+
+public class Role {
+	 private int roleId; // primary key
+	 private String role; // not null, unique	 
+	 
+	 
+	public Role(int roleId, String role) {
+		super();
+		this.roleId = roleId;
+		this.role = role;
+	}
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [roleId=" + roleId + ", role=" + role + ", getRoleId()=" + getRoleId() + ", getRole()=" + getRole()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+	
+	
+
+}
